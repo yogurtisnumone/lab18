ee8d724851413439e982000f41ca39612367f61a
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-02-13 12:47:52 +0700

    18.1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include <iostream>
+#include<cmath>
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+ double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect A, Rect B){
+    double lx = max(0.0, min(A.x + A.w, B.x + B.w) - max(A.x, B.x));
+    double ly = max(0.0, min(A.y, B.y) - max(A.y - A.h, B.y - B.h));
+    return lx * ly;
 }
